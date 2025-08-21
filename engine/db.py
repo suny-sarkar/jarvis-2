31e7840ae13f75eadcb8cf0f0aaa150be48e4fae
suny_sarkar <sonysarkar978@gmail.com>
2025-08-21 17:45:00 +0530

    final-repo

--- engine/db.py
+++ engine/db.py
@@ -24,7 +24,7 @@ cursor.execute(query)
 
 # Create a table with the desired columns
 # cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
-# query = "INSERT INTO contacts VALUES (null,'shweta','+919230746051','null')"
+# query = "INSERT INTO contacts VALUES (null,'','','null')"
 # cursor.execute(query)
 # con.commit()
 
